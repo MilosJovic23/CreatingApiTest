c15f8cfadedce92422e8a46007f8c6302df40ce9
MilosJovic23 <milos.mj992@gmail.com>
2024-12-05 14:42:58 +0100

    * small changes

---
+++ src/ProductGateway.php
@@ -0,0 +1,37 @@
+
+<?php
+
+
+    class ProductGateway {
+
+
+        private PDO $conn;
+        public function __construct(Database $database ) {
+
+            $this->conn = $database->getConnection();
+        }
+        public function getAll():array {
+
+            $sql = "SELECT * FROM product";
+            $stmt = $this->conn->query($sql);
+            $data = [];
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $row['is_available'] = (bool) $row['is_available'];
+                $data[] = $row;
+            }
+            return $data;
+        }
+        public function create(array $data ):string {
+            $sql = "INSERT INTO products (name,size ,is_aliviable)
+                    VALUES (:name,:size,:is_aliviable)";
+
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindValue(':name', $data['name'],PDO::PARAM_STR);
+            $stmt->bindValue(':size', $data['size'] ?? 0,PDO::PARAM_INT);
+            $stmt->bindValue(':is_aliviable', (bool)$data['is_aliviable'] ?? false,
+                PDO::PARAM_BOOL);
+            $stmt->execute();
+
+            return $this->conn->lastInsertId();
+        }
+    }
