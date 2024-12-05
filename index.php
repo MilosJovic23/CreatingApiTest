cdf0e32e3de18d5121958f29fde50e8bc7488c66
MilosJovic23 <milos.mj992@gmail.com>
2024-12-05 14:42:36 +0100

    * small changes

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
 
-    declare(strict_types=1);
 
+    declare(strict_types=1);
 
     spl_autoload_register(function ($class) {
         require __DIR__ . "/src/$class.php";
     });
 
     set_exception_handler("ErrorHandler::handleException");
-
     header("Content-Type: application/json,charset=utf-8");
+
 
     $parts = explode("/",$_SERVER["REQUEST_URI"] );
 
-    $database = new Database("localhost", "root", "root", "");
+    $database = new Database("localhost", "product_db", "root", "");
 
-    $database->getConnection();
+    $gateway = new ProductGateway($database);
+
+    $controller = new ProductController($gateway);
 
 
     if (  $parts[2] !== "products") {
         http_response_code(404);
         exit;
     }
-
     $id = $parts[3] ?? null;
 
-    $controller = new ProductController();
+
     $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
 
 
