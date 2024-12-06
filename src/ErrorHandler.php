8a48b2acc9a6f6fda7d93dd39b7109c116cca6b4
MilosJovic23 <milos.mj992@gmail.com>
2024-12-06 15:37:00 +0100

    * updated errorHandler with method handleError

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -13,4 +13,8 @@
 
                 ]);
         }
+        public static function handleError( int $errno, string $errstr, string $errfile, int $errline):bool {
+
+            throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+        }
     }
