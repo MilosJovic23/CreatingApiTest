e0985116f06eb4aa993b37175f37c0de47e5abf0
MilosJovic23 <milos.mj992@gmail.com>
2024-12-06 15:35:35 +0100

    * added Post case for proccesCollectionRequest method

--- src/ProductGateway.php
+++ src/ProductGateway.php
@@ -6,7 +6,7 @@
 
 
         private PDO $conn;
-        public function __construct(Database $database ) {
+        public function __construct( Database $database ) {
 
             $this->conn = $database->getConnection();
         }
@@ -22,16 +22,16 @@
             return $data;
         }
         public function create(array $data ):string {
-            $sql = "INSERT INTO products (name,size ,is_aliviable)
-                    VALUES (:name,:size,:is_aliviable)";
-
+            $sql = "INSERT INTO product (name,size ,is_available)
+                    VALUES (:name,:size,:is_available)";
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':name', $data['name'],PDO::PARAM_STR);
             $stmt->bindValue(':size', $data['size'] ?? 0,PDO::PARAM_INT);
-            $stmt->bindValue(':is_aliviable', (bool)$data['is_aliviable'] ?? false,
-                PDO::PARAM_BOOL);
+            $stmt->bindValue(':is_available', (bool)$data['is_available'] ?? false,PDO::PARAM_BOOL);
+
             $stmt->execute();
 
             return $this->conn->lastInsertId();
+
         }
     }
