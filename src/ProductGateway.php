0351f9788bfdbb72514418f1c5f648ecc1cb64aa
MilosJovic23 <milos.mj992@gmail.com>
2024-12-07 16:21:33 +0100

    * added get method

--- src/ProductGateway.php
+++ src/ProductGateway.php
@@ -34,4 +34,16 @@
             return $this->conn->lastInsertId();
 
         }
+
+        public function get( string $id ): array | false {
+            $sql = "SELECT * FROM product WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ( $data !== false ) {
+                $data['is_available'] = (bool) $data['is_available'];
+            }
+            return $data;
+        }
     }
