9e538c3ad3a6babe0310992708afc9be20c915f5
MilosJovic23 <milos.mj992@gmail.com>
2024-12-09 15:07:13 +0100

    * updated methods for class

--- src/ProductController.php
+++ src/ProductController.php
@@ -25,7 +25,49 @@
         private function processResourceRequest( string $method, string $id ):void {
 
             $product = $this->gateway->get($id);
-            echo json_encode($product);
+            if ( !$product ) {
+                http_response_code(404);
+                echo json_encode(["message"=>"Product not found"]);
+                return;
+            }
+
+            switch ($method) {
+
+                case "GET":
+                    echo json_encode($product);
+                    break;
+
+                case "PATCH":
+                    $data = (array) json_decode(file_get_contents('php://input'),true);
+                    $errors = $this->getValidationErrors($data);
+
+                    if ( !empty ($errors)) {
+                        http_response_code(422);
+                        echo json_encode(["errors" => $errors]);
+                        break;
+                    }
+
+                    $rows = $this->gateway->update($product,$data);
+                    echo json_encode([
+                        "message" => "Product $id updated",
+                        "rows" => $rows
+                    ]);
+                    break;
+
+                case "DELETE":
+                    $rows = $this->gateway->delete($id);
+                    echo json_encode([
+                        "message" => "Product $id deleted",
+                        "rows" => $rows
+                    ]);
+                    break;
+
+                default:
+                    http_response_code(405);
+                    header("Allow: GET, PATCH, DELETE");
+            }
+
+
         }
         private function processCollectionRequest( string $method ):void {
 
@@ -36,7 +78,7 @@
                 case 'POST':
 
                     $data = (array) json_decode(file_get_contents('php://input'),true);
-                    $errors = $this->getValidationErrors($data);
+                    $errors = $this->getValidationErrors( $data, false );
 
                     if ( !empty ($errors)) {
                         http_response_code(422);
@@ -60,11 +102,11 @@
             }
 
         }
-        public function getValidationErrors ( array $data ):array {
+        public function getValidationErrors ( array $data, bool $is_new = true ):array {
 
             $errors = [];
 
-            if ( empty($data['name']) ) {
+            if ( $is_new && empty($data['name']) ) {
                 $errors[] = "Name is required";
             }
             if ( array_key_exists('size', $data) ) {
